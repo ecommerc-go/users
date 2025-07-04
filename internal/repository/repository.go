da30c090e84f6a8d4ebd61b75ed8f95a9d2228d8
Denis <denis@MacBook-Pro-Denis.local>
2025-07-04 16:53:10 +0300

    добавлены методы get и delete

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -41,3 +41,31 @@ func (r *Repository) CreateUser(ctx context.Context, user *service.RegisterUserR
 		Id: string(createdUser),
 	}, nil
 }
+
+func (r *Repository) GetUser(ctx context.Context, id string) (*service.GetProfileResponse, error) {
+	query := `SELECT email, name, address 
+	          FROM users WHERE id=$1`
+
+	var profile service.UserProfile
+	err := r.conn.QueryRowContext(ctx, query,
+		id).
+		Scan(&profile.Email, &profile.Name, &profile.Address)
+	fmt.Println(err)
+	fmt.Println()
+	if err != nil {
+		return nil, errors.New("failed to create user")
+	}
+	return &service.GetProfileResponse{
+		Profile: profile,
+	}, nil
+}
+
+func (r *Repository) DeleteUser(ctx context.Context, id string) error {
+	query := `DELETE FROM users WHERE id=$1;`
+
+	_, err := r.conn.ExecContext(ctx, query, id)
+	if err != nil {
+		return errors.New("user dont delete")
+	}
+	return nil
+}
